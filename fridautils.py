9d97b116495ee729288e6fcd27367a84d5d86a09
smartdone <smartdone@outlook.com>
2019-12-12 22:00:24 +0800

    add widget

--- fridautils.py
+++ fridautils.py
@@ -4,6 +4,7 @@
 # @Time    : 2019/12/12 20:08
 
 import frida
+from PyQt5.QtWidgets import QMessageBox
 
 
 def singleton(cls):
@@ -20,5 +21,29 @@ def singleton(cls):
 @singleton
 class FridaUtils(object):
     def __init__(self):
-        self.devices = frida.enumerate_devices()
+        self.current_device = None
+        self.current_process = None
+        self.current_package = None
+        self.current_platform = None
+        self.session = None
 
+        self.devices = {}
+
+    def get_device_list(self):
+        self.devices = {}
+        for item in frida.enumerate_devices():
+            key = "{name}:{id}".format(name=item.name, id=item.id)
+            self.devices[key] = item
+        return self.devices
+
+    def set_current_device(self, device_name):
+        if self.devices.keys().__contains__(device_name):
+            self.current_device = self.devices[device_name]
+        else:
+            QMessageBox.critical("错误", "设备断开，或者无法附加，尝试使用管理员权限运行")
+
+    def get_process_list(self):
+        pass
+
+    def set_current_process(self):
+        pass
--- ui.py
+++ ui.py
@@ -10,55 +10,56 @@ class FridaUI(QMainWindow):
     def __init__(self):
         super().__init__()
         self.frida = FridaUtils()
-        self.setup_ui()
-        self.show()
-
-    def setup_ui(self):
-
-        self.statusBar().showMessage('Ready')
         self.setWindowTitle("Frida UI")
         self.resize(1440, 900)
         self.setWindowIcon(QIcon("frida.png"))
-        root = QVBoxLayout()
+        self.root = QVBoxLayout()
         device_bar = QHBoxLayout()
-        root.addLayout(device_bar)
+        self.root.addLayout(device_bar)
 
         device_label = QLabel("设备")
-        device_list = QComboBox()
-        device_refresh = QPushButton("刷新设备")
+        self.device_list = QComboBox()
+        self.device_refresh = QPushButton("刷新设备")
         device_bar.addWidget(device_label)
-        device_bar.addWidget(device_list)
-        device_bar.addWidget(device_refresh)
+        device_bar.addWidget(self.device_list)
+        device_bar.addWidget(self.device_refresh)
+        self.device_list.currentIndexChanged.connect(self.select_device_action)
 
         process_label = QLabel("进程")
-        process_list = QComboBox()
-        process_refresh = QPushButton("刷新进程")
-        attach_process_btn = QPushButton("附加进程")
+        self.process_list = QComboBox()
+        self.process_refresh = QPushButton("刷新进程")
+        self.attach_process_btn = QPushButton("附加进程")
         process_pid_label = QLabel("进程id")
-        process_pid_edt = QLineEdit()
+        self.process_pid_edt = QLineEdit()
         device_bar.addWidget(process_label)
-        device_bar.addWidget(process_list)
-        device_bar.addWidget(process_refresh)
+        device_bar.addWidget(self.process_list)
+        device_bar.addWidget(self.process_refresh)
         device_bar.addWidget(process_pid_label)
-        device_bar.addWidget(process_pid_edt)
-        device_bar.addWidget(attach_process_btn)
+        device_bar.addWidget(self.process_pid_edt)
+        device_bar.addWidget(self.attach_process_btn)
 
-        for item in self.frida.devices:
-            device_list.addItem("{name}:{id}".format(name=item.name, id=item.id))
+        for item in self.frida.get_device_list():
+            self.device_list.addItem(item)
 
         app_package_label = QLabel("应用包名")
-        app_package_edt = QLineEdit()
-        app_spawn_btn = QPushButton("启动应用")
+        self.app_package_edt = QLineEdit()
+        self.app_spawn_btn = QPushButton("启动应用")
         device_bar.addWidget(app_package_label)
-        device_bar.addWidget(app_package_edt)
-        device_bar.addWidget(app_spawn_btn)
+        device_bar.addWidget(self.app_package_edt)
+        device_bar.addWidget(self.app_spawn_btn)
 
         # 将多余部分填充空白
         device_bar.addStretch(1)
 
-        root.addStretch(1)
+        self.root.addStretch(1)
 
         widget = QWidget()
-        widget.setLayout(root)
+        widget.setLayout(self.root)
         self.setCentralWidget(widget)
+        self.show()
+
+    def select_device_action(self):
+        device = self.device_list.currentText()
+        self.statusBar().showMessage("选择设备: {}".format(device))
+        self.frida.set_current_device(device)
 
